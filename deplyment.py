075941c1ec819fdb3892bc8e447bfd1fd4d9bf84
slimisarra <47771183+slimisarra@users.noreply.github.com>
2022-11-12 15:03:02 +0100

    Update deplyment.py

--- deplyment.py
+++ deplyment.py
@@ -16,6 +16,7 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+nltk.download('stopwords')
 
 DATASET_FILE = "./dataset_cleaned.csv"
 dataset_df = pd.read_csv(DATASET_FILE)
